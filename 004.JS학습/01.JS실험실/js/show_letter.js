007494d49243a0a975f7fc3f6b1c8dbc4dfe35aa
mimimgg <com42123@gmail.com>
2024-12-05 10:45:00 +0900

    20241205_41days

---
+++ 004.JS학습/01.JS실험실/js/show_letter.js
@@ -0,0 +1,32 @@
+// JS실험실 : 01.글자등장1 //
+
+// 나의 함수 가져오기
+import myFn from './my_function.js';
+
+// 1. 요구사항 분석
+// - 글자를 박스에 넣고 하나씩 일어나면서 등장
+
+// 2. 대상선정
+// - 글자를 넣을 박스
+const stage = myFn.qs('.stage-letters');
+console.log(stage);
+
+// 3. 변경내용 적용하기
+// - 박스에 글자를 span으로 감싸서 하나씩 모두 넣기
+
+// 글자 내용 변수
+const myText = [
+  '너의 췌장을 먹고싶어😋',
+  '추락하는 것은 날개가 있다🪽',
+  '뻐꾸기 둥지 위로 날아간 새🐦'
+];
+
+// 한글자씩 잘라서 순회하는 제어문은? for of
+let result = "";
+for(let x of myText) {
+  // console.log(x);
+  result += `<span>${x}</span>`;
+} // for of //
+
+// 4. 결과 출력
+stage.innerHTML = result;
