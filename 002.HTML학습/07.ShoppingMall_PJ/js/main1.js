00804f5caa562c9906e868d3f699e105f1a97eea
mimimgg <com42123@gmail.com>
2024-12-06 16:10:35 +0900

    20241206_42days

--- 002.HTML학습/07.ShoppingMall_PJ/js/main1.js
+++ 002.HTML학습/07.ShoppingMall_PJ/js/main1.js
@@ -30,7 +30,6 @@ import myFn from "./my_function.js";
 *****************************************************/
 
 // 전역변수구역 //////////
-
 // (1) 순번변수
 let seqNum = 0;
 
@@ -51,6 +50,10 @@ const SLIDE_CNT = myFn.qsaEl(slide,'li').length;
 console.log(SLIDE_CNT);
 
 // console.log("대상",slideBox,abtn,slide);
+
+// (4) 인디케이터 블릿대상
+const indic = myFn.qsaEl(slideBox,'.indic li');
+// const indic = myFn.qsaEl(slideBox,'.indic li');//
 
 // 2. 이벤트설정하기
 abtn.forEach(el=>{
@@ -112,6 +115,27 @@ function goSlide(){
         abtn[1].style.display = "block";
     } // else
     
+    
+  // 6. 인디케이터 변경하기 : 대상 .indic li
+  indic.forEach((el, idx) => {
+    // console.log(el,idx);
+    // (1) 페이지번호와 일치하는 순번li에 클래스 "on"넣기
+    if (idx === seqNum) {
+      el.classList.add("on");
+    } /// if ///
+    // (2) 나머지 li는 "on" 제거하기
+    else {
+      el.classList.remove("on");
+    } /// else ///
+  }); /// forEach ////
+
+  // -> seqNum 값 즉, 슬라이드 순번과
+  // 인디케이터 li 순번이 같으므로
+  // 해당순번의 li에 클래스"on"을 넣고
+  // 나머지는 "on"을 제거한다!
+    
+    // -> seqNum 값 즉, 슬라이드 순번과 인디케이터 li순번이 같으므로
+    // 해당 순번의 li에 closs'on'을 넣고 나머지는 on을 제헌다.]
 } // goSlide //
 
 
