95288448423d8def9f799ff375be960c88aa88dd
mimimgg <com42123@gmail.com>
2024-12-11 16:45:01 +0900

    20241211_45days

--- 002.HTML학습/07.ShoppingMall_PJ/js/unlimit.js
+++ 002.HTML학습/07.ShoppingMall_PJ/js/unlimit.js
@@ -71,6 +71,8 @@ abtn.forEach(el=>{
     // 각 요소에 이벤트 설정하기
     // click이벤트를 addEventListener로 설정
     myFn.addEvt(el,'click',goSlide);
+    // 이동버튼 클릭시 인터발 지우기 함수 호출 //
+    myFn.addEvt(el, 'click', ()=>{clearAuto()});
 }); // forEach //
 
 // 광클 금지 상태 변수
@@ -88,10 +90,17 @@ function goSlide(){
     }, TIME_ANI)
 
     // 1. 함수호출확인
-    // console.log('나함수',this);
+    console.log('나함수',this, this.classList);
     
-    // 2. 오른쪽버튼 여부 확인
-    let isRight = this.classList.contains('ab2');
+    // 2. 오른쪽버튼 여부 확인 (기본값 true 할당)
+    let isRight = true;
+
+    // 만약 this.classList가 undefined가 아니면 클래스 오른쪽 여부를 판단한다
+    if(this.classList)
+      isRight =  this.classList.contains('ab2');
+    // 일반적으로 버튼을 클릭하지 않고 호출하면 window객체가 this로 잡히므로 classList객체가 없어서 undefined로 나오고, 하위 클래스인 contains()가 없으므로 에러메시지가 나온다. 따라서 이런 경우를 대비하여 기본값으로 isRight에 true를 주고, 직접 호출 시 오른쪽으로 이동하게 해준다. 만약 버튼을 클릭하게 되면 if문에서 걸러주므로 실제 버튼 클래스 존재 여부를 판단하여 오른쪽/왼쪽 이동버튼 분기가 작동된다.
+
+    // classList.contains(클래스명) -> 클래스 있으면 true
     console.log('나함수',isRight);
     
     // 3. 현재 변경된 li 수집
@@ -181,5 +190,28 @@ function goSlide(){
 } // goSlide //
 
 
+/*********************************************
+              인터벌 호출 설정하기
+*********************************************/
+// 인터벌설정을 지우기 위해 변수에 할당한다
+let autoI, autoT;
 
+// 인터발함수 최초호출
+autoSlide();
+
+// [1] 인터발 설정 함수
+function autoSlide(){
+  autoI = setInterval(goSlide, 2000);
+} // autoSlide 함수 //
+
+// [2] 인터벌 지우기 함수
+// -> 버튼 직접 조작시 호출함
+function clearAuto(){
+  // (1) 인터발 지우기
+  clearInterval(autoI);
+  // (2) 타임아웃 지우기(실행 쓰나미 방지)
+  clearTimeout(autoT);
+  // (3) 일정시간이 지난 후 인터벌 재호출 (최종 하나만 남김)
+  autoT = setTimeout(autoSlide, 4000);
+} // clearAuto 함수 //
 
--- 004.JS학습/01.JS실험실/js/show_letter.js
+++ 004.JS학습/01.JS실험실/js/show_letter.js
@@ -1,65 +1,72 @@
-// JS실험실 : 01.글자등장1 //
+// JS실험실 : 01.글자등장1 JS - show_letter.js
 
-// 나의 함수 가져오기
+// 나의 함수 가져오기 ////
 import myFn from "./my_function.js";
 
 // 1. 요구사항 분석
 // - 글자를 박스에 넣고 하나씩 일어나면서 등장
 
 // 2. 대상선정
-// - 글자를 넣을 박스 (3개임)
+// 글자를 넣을 박스(3개임!)
 const stage = myFn.qsa(".stage-letters");
-console.log(stage);
+
+console.log("대상:", stage);
 
 // 3. 변경내용 적용하기
-// - 박스에 글자를 span으로 감싸서 하나씩 모두 넣기
-// - 스페이스 문자는 b태그로 변환한다.
+// - 박스에 글자를 span으로 싸서 하나씩 모두넣기
+// - 스페이스 문자는 b태그로 변환한다!
 
-// 글자 내용 배열 변수
-const myText = ["너의 췌장을 먹고싶어😋", "추락하는 것은 날개가 있다🪽", "뻐꾸기 둥지 위로 날아간 새🐦"];
+// 글자내용 배열변수
+const myText = [
+  "너의 췌장을 먹고싶어🐷",
+  "추락하는 것은 날개가 있다🦅",
+  "뻐꾸기 둥지 위로 날아간 새🐓",
+];
 
-// 배열만큼 돌돌면서 세팅하기 : forEach() 메서드
+// 배열만큼 돌면서 셋팅하기 : forEach() 메서드
 myText.forEach((v, i) => {
+  // v - 배열값, i - 순번
   console.log(v, i);
   // 한글자씩 잘라서 순회하는 제어문은? for of
   let result = ""; // 결과변수
   let dTime = 0; // 지연시간변수
   for (let x of v) {
-    // v는 배열값
+    // v 배열값
     // console.log(x);
 
-    // 스페이스 값일 때 b태그 넣기
+    // 스페이스값일때 b태그 넣기
     if (x == " ") result += `<b></b>`;
-    // 문자값이면 span으로 싸기 + 트랜지연 지연시간
-    // 지연시간은 0.08초에 0,1,2,3,4,...을 순서대로 곱함
+    // 문자값이면 span으로 싸기 + 트랜지션 지연시간
+    // 지연시간은 0.08초에 0,1,2,3,...을 순서대로 곱한다
     else {
       result += `
-      <span style="transition-dalay:${0.08 * dTime}s">
-      ${x}</span>`;
+        <span 
+        style="transition-delay:${0.08 * dTime}s">
+        ${x}</span>`;
 
-      // 다음 순회시 dTime 1씩 증가
-      console.log(dTime);
+      // 다음순회시 dTime 1씩증가하기 - 문자일때만!
+      console.log("지연시간곱할수:", dTime);
       dTime++;
     }
-  } // for of //
+  } //// for of ///
 
-  // 4. 결과 출력 : stage는 html컬렉션이다
-  // -> .stage-letters 3개가 담겨있음
-  // -> 배열의 순번과 같은 순번에 html 넣기
+  // 4. 결과 출력 : stage는 html컬렉션이다!
+  // -> .stage-letters 3개가 담겨있음!
+  // -> 순번은 배열의 순번과 같은 순번에 html넣기
   stage[i].innerHTML = result;
 
-  // 5. 글자박스에 클래스 style1, style2, style3
+  // 5. 글자박스에 클래스 style1,style2,style3 넣기
   stage[i].classList.add("style" + (i + 1));
-}); // forEach //
+}); ///////// forEach //////////
 
-// 6. 일정시간 후 stage에 클래스 On 넣어서 글자 등장하기
-// 시간세팅 배열변수
-const startTime = [2000, 3000, 5000];
+// 6. 일정시간후 stage에 클래스 on넣어서 글자등장하기
+// 시간셋팅 배열변수
+const startTime = [2000, 3500, 5000];
 stage.forEach((el, idx) => {
-  // el - 출력할요소, idx - 요소순번
-  console.log("요소:", el, "순번:", idx);
-  // 타임아웃 세팅하기
+  // el - 출력할요소 , idx - 요소순번
+  console.log("요소:", el, "/순번:", idx);
+  // 타임아웃 셋팅하기 ///
   setTimeout(() => {
     el.classList.add("on");
   }, startTime[idx]);
-});
+}); /// forEach ////
