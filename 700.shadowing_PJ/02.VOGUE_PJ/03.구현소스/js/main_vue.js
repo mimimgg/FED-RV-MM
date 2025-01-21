9774da6590ab12557399cde2625b1deff2a04500
mimimgg <com42123@gmail.com>
2025-01-21 09:37:12 +0900

    20250121

---
+++ 700.shadowing_PJ/02.VOGUE_PJ/03.구현소스/js/main_vue.js
@@ -0,0 +1,23 @@
+// 보그 PJ 메인페이지 뷰 적용 JS - main_vue.js // 
+
+// 컴포넌트 불러오기
+// [1] 공통컴포넌트 common.js
+import { TopComp } from "./components/common.js";
+console.log(TopComp);
+
+/*********************************************
+      메인 뷰 인스턴스 생성하기
+*********************************************/
+new Vue ({
+  // 1. 대상설정
+  el: '#vogue-app',
+  // 2. 데이터설정
+  data: {},
+  // 3. 메서드 설정
+  methods: {},
+  // 4. 라이프사이클 메서드
+  // 4-1. created() : 데이터 생성 관련 코드 작성
+  created(){},
+  // 4-2. mounted() : DOM 관련 코드 작성
+  mounted(){},
+})
