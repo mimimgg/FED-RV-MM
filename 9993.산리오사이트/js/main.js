b156463a5e98911110c5aebb4acee15d74aa9e66
mimimgg <com42123@gmail.com>
2025-01-31 14:15:57 +0900

    20250131

--- 9993.산리오사이트/js/main.js
+++ 9993.산리오사이트/js/main.js
@@ -6,9 +6,20 @@ import myFn from './my_function.js';
 import catData from './cat_data.json' with {type:'json'};
 console.log(catData);
 
+// GNB용 데이터 배열 
+const gnbData = ["company","character","place","recruit"];
+
 // 1. gnb 메뉴 글자변경 (uppercase) //
-document.querySelectorAll(".gnb ul li a")
-.forEach(el=>el.innerText=el.innerText.toUpperCase());
+document.querySelector(".gnb").innerHTML = `
+  <ul>
+  ${gnbData.map( v => `
+      <li>
+        <a href="#">${v}</a>
+      </li>
+    `).join('')}
+  </ul>
+`;
+// .forEach((el)=>el.innerText=el.innerText.toUpperCase());
 
 // 모바일 모드에서 GNB 디자인을 모바일로 변경하였으므로 보여주는 코드는 클래스 'show'를 넣어서 햄버거 버튼 기능과 연관시킨다.
 
@@ -27,42 +38,106 @@ document.querySelector(".gnb-mob").onclick=function(){
 document.querySelector('.cat-list').innerHTML =
 `
   <ul>
-  ${catData.map(v => `
-    <li>
-      <figure>
-        <div class="img-part">
-          <img src="./images/${v.isrc}.png" alt="${v.title}">
-          <img src="./images/${v.isrc}_ov.png" alt="${v.title}">
-        </div>
-        <figcaption>
-          <h3>${v.title}</h3>
-          <p class="title-en">${v.title_en}</p>
-        </figcaption>
-      </figure>
-    </li>
+  ${catData.map(
+    (v) => `
+      <li>
+        <figure>
+          <div class="img-part">
+            <img src="./images/${v.isrc}.png" alt="${v.title}">
+            <img src="./images/${v.isrc}_ov.png" alt="${v.title}">
+          </div>
+          <figcaption>
+            <h3>${v.title}</h3>
+            <p class="title-en">${v.title_en}</p>
+          </figcaption>
+        </figure>
+      </li>
     `).join('')}
   </ul>
-`
+`;
 
 
-// 4. company 영역 
+// 4. 컴퍼니 영역 
 // 데이터 : 배열
 const companyData = [
   {
     title: "회사소개",
-    content: "맑고 깨끗한 강가에 문화를 쌓아 올리자.",
-    imgsrc: "ico_company1.png"
+    content: "맑고 깨끗한 강가에^ 문화를 쌓아 올리자.",
+    imgsrc: "ico_company1"
   },
   {
     title: "회사연혁",
-    content: "산리오의 히스토리를 확인하세요.",
-    imgsrc: "ico_company2.png"
+    content: "산리오의 히스토리를^ 확인하세요.",
+    imgsrc: "ico_company3"
   },
   {
     title: "사업영역",
-    content: "산리오의 히스토리를 확인하세요.",
-    imgsrc: "ico_company3.png"
+    content: "소셜 커뮤니케이션을^ 산리오가 시작합니다..",
+    imgsrc: "ico_company4"
   }
 ];
 
-console.log(companyData);
+// 4. 컴퍼니 영역 데이터 연결하여 출력태그 만들기
+document.querySelector('.company-list').innerHTML =
+`
+  <ul class="com-type-list">
+  ${companyData.map(
+    (v) => `
+      <li>
+        <figure>
+          <div class="img-part">
+            <img src="./images/${v.imgsrc}.png" alt="${v.title}">
+          </div>
+          <figcaption>
+            <h3>${v.title}</h3>
+            <p class="title-en">${v.content.split('^')[0]}</p>
+            <p class="title-en">${v.content.split('^')[1]}</p>
+            <a href="#" class="com-btn">more</a>
+          </figcaption>
+        </figure>
+      </li>
+    `).join('')}
+  </ul>
+`
+
+// 5. 리쿠르트 영역 ///////////////////////
+// 데이터 : 배열
+const recruitData = [
+  {
+    title: "채용공고",
+    content: "산리오코리아에서^ 인재를 찾습니다",
+    imgsrc: "ico_recruit1",
+  },
+  {
+    title: "직무소개",
+    content: "산리오코리아에서는^ 어떤 일을 할까요?",
+    imgsrc: "ico_recruit2",
+  },
+  {
+    title: "복리후생",
+    content: "산리오코리아에서^ 누릴 수 있는것!",
+    imgsrc: "ico_recruit3",
+  },
+];
+// 5. 리쿠르트 영역 데이터 연결하여 출력태그 만들기
+document.querySelector('.recruit-list').innerHTML =
+`
+  <ul class="com-type-list">
+  ${recruitData.map(
+    (v) => `
+      <li>
+        <figure>
+          <div class="img-part">
+            <img src="./images/${v.imgsrc}.png" alt="${v.title}">
+          </div>
+          <figcaption>
+            <h3>${v.title}</h3>
+            <p class="title-en">${v.content.split('^')[0]}</p>
+            <p class="title-en">${v.content.split('^')[1]}</p>
+            <a href="#" class="com-btn">more</a>
+          </figcaption>
+        </figure>
+      </li>
+    `).join('')}
+  </ul>
+`
