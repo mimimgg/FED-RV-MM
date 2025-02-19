78c7bfb94d0f1b887de79fd671f56f4bb466224f
mimimgg <com42123@gmail.com>
2025-02-19 12:13:34 +0900

    20250219

--- 008.React학습/01.react-cdn/js/07.조건렌더링.jsx
+++ 008.React학습/01.react-cdn/js/07.조건렌더링.jsx
@@ -176,21 +176,35 @@ function MakeList({foodName, movieInfo}){
 } // MakeList 컴포넌트 //
 
 // 2-3. 개발자 선호음식 리스트 출력 컴포넌트
-function WishList ({wList}) {
+function WishList ({wList, gubun}) {
+  // wList - 좋아하는 음식 리스트 (배열)
+  // gubun - 음식/영화 구분 문자값
   return (
     <React.Fragment>
       {/* 음식 리스트 타이틀 */}
-      <SetTitle title="음식"/>
+      <SetTitle title={gubun}/>
       { // 표현식 코드구역의 태그는 부모가 하나여야함
       // 배열값이 0개 이상인 경우 출력
       wList.length > 0 &&
         <div>
-          <h2>
-            개발자가 좋아하는 음식은 모두 
-            {wList.length}가지 입니다.
-          </h2>
+          { // 구분값이 "음식"인 경우 
+          gubun == "음식" && 
+            <h2>
+              개발자가 좋아하는 음식은 모두 
+              {wList.length}가지 입니다.
+            </h2>
+          }
+          { // 구분값이 "영화"인 경우 
+          gubun == "영화" && 
+            <h2>
+              개발자가 좋아하는 영화는 최근 
+              {wList.length}년간 아래와 같습니다.
+            </h2>
+          }
+          
           <ul>
-            {
+            { // 구분값이 "음식"인 경우
+            gubun == "음식" &&
               wList.map(v => 
               <MakeList foodName={v}/>)
 
@@ -198,14 +212,29 @@ function WishList ({wList}) {
               map메서드는 원래 새로운배열을 현재 자리에 출력하는 용도임 그러나 리액트는 이것을 변경하여 표현식안에서 출력시 태그JSX 형식으로 변환해 줌!
               JS 처럼 map().join('')처리 불필요!!!*/
             }
+            { // 구분값이 "영화"인 경우
+            gubun == "영화" &&
+              wList.map(v => 
+              <MakeList movieInfo={v}/>)
+            }
           </ul>
+          { // 구분값이 "영화"인 경우 포스터 출력
+            gubun == "영화" &&
+              wList.map(v => 
+                <MakeImage 
+                  isrc = {v.poster}
+                  ialt = {v.mtit}
+                  icss = {{width: "16%", marginLeft: "0.5%"}}
+                />
+              )
+          }
         </div>
       }
 
       {/* { 배열개수가 0인 경우 다른 것을 출력하기 */}
       { 
         wList.length == 0 && (
-          <h2>아직 개발자 음식 리스트가 업데이트 되지 않았습니다. </h2>
+          <h2>아직 개발자 {gubun} 리스트가 업데이트 되지 않았습니다. </h2>
         )
       }
     </React.Fragment>
@@ -213,7 +242,156 @@ function WishList ({wList}) {
 } // WishList 컴포넌트 //
 
 // 음식 배열값이 있는 경우 출력하기
-ReactDOM.render(<WishList wList={foods}/>, root[2]);
+ReactDOM.render(<WishList wList={foods} gubun="음식"/>, root[2]);
 
 // 음식 배열값이 없는 경우 출력하기
-ReactDOM.render(<WishList wList={[]}/>, root[3]);
+ReactDOM.render(<WishList wList={[]} gubun="음식"/>, root[3]);
+
+/* 
+[ 출력형태 ]
+    👨‍🔧개발자👩‍🔧가 좋아하는 영화
+    개발자가 좋아하는 영화는 최근 몇년간 아래와 같습니다!
+    2021년도 영화1
+    2022년도 영화2
+    2023년도 영화3
+    ... 여기는 영화포스트 나열하기 ...
+*/
+
+// 개발자가 선호하는 영화 리스트 출력 컴포넌트
+// 제목 컴포넌트, 리스트 컴포넌트, 위시리스트 컴포넌트 모두 재활용한다.
+
+// 영화 위시리스트 출력하기 : 배열값이 있는 경우
+ReactDOM.render(
+<WishList wList={movs} gubun="영화"/>, root[4]);
+
+// 영화 위시리스트 출력하기 : 배열값이 없는 경우
+ReactDOM.render(
+<WishList wList={[]} gubun="영화"/>, root[5]);
+
+/********************************************************** 
+    4. 조건 연산자(삼항연산자)를 사용하여 조건부 랜더링하기 
+    비?집:놀이동산
+**********************************************************/
+// 명화 데이터
+const workUrl = {
+  피카소:
+    "https://m.theartin.net/web/product/big/201907/30c5a0fdd153bfdfdc8f19b2f4166fa8.jpg",
+  모네: "https://dimg.donga.com/wps/NEWS/IMAGE/2015/12/11/75316598.3.jpg",
+};
+
+// 개발자가 좋아하는 그림(명화) 찍기
+
+// 4-1. 작가타이틀과 그림찍기 컴포넌트 //////
+// 구성 : 작가이름 + 작품이미지
+// 데이터 : 작가이름 (painter)
+//          이미지경로(작가이름의 객체 - workUrl)
+//          작품명(wname)
+
+function MakePainting ({ painter, wname }) {
+  // painter - 작가이름
+  // wname - 작품명
+
+  // 코드리턴 구역 //
+  return (
+    <div>
+      {/* 작가이름 타이틀 */}
+      <h2>{ painter }</h2>
+      {/* 이미지 태그 출력 */}
+      <img 
+        src = {workUrl[painter]} 
+        alt = {painter + "의 작품:" + wname}
+        style = {{width: "400px"}}
+        title = {wname}
+      />
+    </div>
+  )
+} // MakePainting 컴포넌트 //
+
+// 4-2. 전체출력 컴포넌트 //////////////
+// 구성 : 전체타이틀(SetTitle 컴포넌트) + 변경버튼
+//      + 작가타이틀과 그림출력(MakePainting 컴포넌트)
+// 특이사항 : 변경버튼 클릭시 MakePainting 컴포넌트의
+// 데이터를 변경하여 다시 출력하도록 함!(Hook사용!)
+function ShowLikePainter ({isChange}) {
+  // isChange - 기존 작가를 변경하는 여부 전달변수
+  // true / false -> 이 값에 따라 출력작가가 변경됨
+
+  // 리턴코드구역
+  return (
+    <React.Fragment>
+      {/* 1. 큰제목 */}
+      <SetTitle title="명화" />
+
+      {/* 변경버튼 */}
+      <button 
+        style = {{
+          fontSize: "30px",
+          padding: "10px",
+          fontWeight: "blod",
+          backGroundCOlor: "blue",
+          color: "#ffffff"
+        }}
+      > 작가 변경 </button>
+
+      {/* 3. 작품출력 */}
+      {
+        // 삼항연산자로 isChange 값에 따라 출력변경하기
+        isChange ? 
+        <MakePainting
+          painter = "모네"
+          wname = "양산을 쓴 여인"
+        /> : 
+        <MakePainting
+          painter = "피카소"
+          wname = "우는 여인"
+        />
+      }
+    </React.Fragment>
+  );
+} // ShowLikePainter 컴포넌트 //
+
+// 4-3. 컴포넌트 호출하기
+ReactDOM.render(<ShowLikePainter isChange={false} />, root[6]);
+
+/********************************************************* 
+    [ 리액트 훅크 : React Hook ]
+    - 일반적으로 리액트에 사용되는 변수는 처음에 
+    컴포넌트에 전달되어 초기 셋팅에 활용된다.
+    그런데 이 변수가 변경될 경우 컴포넌트의 변경이
+    자동적으로 이루어지지 않는다!
+    이런 종류의 변수 업데이트가 가상돔과 실제돔에
+    바로 반영되도록 실시간 감시역할을 하는
+    리액트의 기술내용을 담고 있는 것이 후크다!
+
+    1. 목적 : 어떤 특정 데이터가 변경될때
+        이 데이터를 할당하여 사용하고 있는 컴포넌트의
+        변경이 반영되도록 하고자 할때 후크를 사용한다!
+
+    2. 구현방법:
+        1) 노드JS SPA 개발환경에서는 상단에 
+        import useState를 한다!
+        -> CDN 에서는 React.useState 로 사용함!
+        2) 코딩법 : useState() 메서드사용
+            배열변수 = useState(초기값)
+            (CDN) -> 배열변수 = React.useState(초기값)
+
+            ((일반형))
+            const [변수명,set변수명] = useState(초기값)
+            -> set변수명 작성시 변수명 첫글자는 대문자로 씀
+            예) 변수명 myname -> setMyname
+            -> set변수명(값) : 메서드형태로 후크변수의 값을 셋팅함!
+
+        3) 작동원리 
+            - useState에 쓴 초기값이 배열변수 첫번째변수에 할당된다!
+            - 코드에서 set변수명에 값을 할당하면
+            useState메서드가 이것을 체크하여 이 변수를 사용한
+            다른부분의 업데이트를 실행한다!
+            예컨데 컴포넌트 내부에 사용한 경우 컴포넌트가 업데이트 됨!
+        4) 사용결과
+            - 별도의 메서드 호출없이 후크 상태변수를 사용한 곳이
+            자동으로 변경될대마다 다시 갱신되는 것을 확인 할 수 있다!
+
+        -> 뷰JS의 리액티브 데이터와 매우 유사함!
+
+*****************************************************************/
+
