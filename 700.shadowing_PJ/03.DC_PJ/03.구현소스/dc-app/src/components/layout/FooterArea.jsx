f6e317d2db31dd62a4aff9434a29a4936e0ab2cb
mimimgg <com42123@gmail.com>
2025-02-20 17:48:51 +0900

    20250220

---
+++ 700.shadowing_PJ/03.DC_PJ/03.구현소스/dc-app/src/components/layout/FooterArea.jsx
@@ -0,0 +1,9 @@
+// 하단영역 컴포넌트 : FooterArea.jsx
+export default function FooterArea(){
+  // 리턴코드구역
+  return (
+    <>
+      <h1>여기에 작성하면 됩니다.</h1>
+    </>
+  );
+} // FooterArea 컴포넌트 //
---
+++ 700.shadowing_PJ/03.DC_PJ/03.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -0,0 +1,9 @@
+// 상단영역 컴포넌트 : TopArea.jsx
+export default function TopArea(){
+  // 리턴코드구역
+  return (
+    <>
+      <h1>컴포넌트영역</h1>
+    </>
+  );
+} // TopArea 컴포넌트 //
