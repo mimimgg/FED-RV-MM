bd3e9e36acbd3c845712a29ea9049f3d4baa0065
mimimgg <com42123@gmail.com>
2025-02-24 15:49:49 +0900

    20250224

--- 008.React학습/02.react-ex/js/components/GoodsList.jsx
+++ 008.React학습/02.react-ex/js/components/GoodsList.jsx
@@ -13,22 +13,31 @@ import hjData from "../data/hj_data";
 // 공통함수 불러오기
 import * as comFn from "./common/com_fn";
 
-export default function GoodsList({selItem, setGIdx, setViewList}) {
+export default function GoodsList({ selItem, setGIdx, setViewList }) {
   // selItem - 대분류(공유/효진) -> 데이터선택
   // setGIdx - 부모의 상태관리변수 gIdx 업데이트 메서드
   // setViewList - 부모의 상태관리변수 viewList 업데이트 메서드
 
   // 데이터 종류 선택하기
-  const selDB = 
-  selItem === '공유' ? 
-  guData : 
-  selData === '효진' ? 
-  hjData : null;
+  const selDB = selItem === "공유" ? guData : selItem === "효진" ? hjData : null;
 
   // 조건 랜더링 : Null값일 경우
-  if(!selDB) return <ul>
-    <li>데이터가 없습니다.</li>
-  </ul>
+  if (!selDB)
+    return (
+      <ul>
+        <li>데이터가 없습니다.</li>
+      </ul>
+    );
+
+    // [ useEffect 코드구역 : 화면 업데이트 후 실행구역 ]
+    React.useEffect(()=>{
+      console.log("⭕️ 리스트컴포넌트")
+
+      // 컴포넌트 소멸시 실행구역은 useEffect 함수안에 return() 메서드를 만들어준다.
+      return(()=>{
+        console.log("❌ 리스트 컴포넌트 소멸 시 실행되는 것")
+      });
+    }); // useEffect //
 
   // 리턴 코드 구역
   // ⭐️ ul 아래 li가 표현식 구역이 된다. (반복될 데이터를 붙여넣으면 된다.)
@@ -41,28 +50,27 @@ export default function GoodsList({selItem, setGIdx, setViewList}) {
         // 반복 요소 구분을 위한 필수키로 리액트에서 설정할 것을 강제하고 있음(cdn에서는 에러나지 않음)
         selDB.map((v, i) => (
           <li key={i}>
-            <a href="#" 
-            onClick={(e)=>{
-              // 기본이동막기
-              e.preventDefault();
-              // 확인 - idx값 찍기
-              console.log("나는클릭이다", v.idx);
-              // 상태관리변수 gIdx를 업데이트하기
-              setGIdx(v.idx);
-              // viewList 업데이트하기
-              setViewList(false);
-              // false값으로 변경시 상세페이지만 보임
-            }}>
+            <a
+              href="#"
+              onClick={(e) => {
+                // 기본이동막기
+                e.preventDefault();
+                // 확인 - idx값 찍기
+                console.log("나는클릭이다", v.idx);
+                // 상태관리변수 gIdx를 업데이트하기
+                setGIdx(v.idx);
+                // viewList 업데이트하기
+                setViewList(false);
+                // false값으로 변경시 상세페이지만 보임
+              }}
+            >
               <ol className="glist">
                 <li>
-                  {
-                    selItem === "공유" ? 
-                    <img 
-                    src={"./images/vans/vans_"+v.idx+".jpg"} alt="신발" />
-                    : 
-                    <img 
-                    src={"./images/gallery/"+v.idx+".jpg"} alt="드레스" />
-                  }
+                  {selItem === "공유" ? (
+                    <img src={"./images/vans/vans_" + v.idx + ".jpg"} alt="신발" />
+                  ) : (
+                    <img src={"./images/gallery/" + v.idx + ".jpg"} alt="드레스" />
+                  )}
                 </li>
                 <li>🥾상품명 : {v.gname}</li>
                 <li>👟가격 : {comFn.addCommas(v.gprice)}원</li>
