9a666c2ce568bc173f9e281d3a06ac7c091c21b2
mimimgg <com42123@gmail.com>
2025-02-26 11:15:15 +0900

    달력 너무 어렵당

--- 004.JS학습/08.JS달력/01.기본기능/js/calendar.js
+++ 004.JS학습/08.JS달력/01.기본기능/js/calendar.js
@@ -41,6 +41,11 @@ function makeDallyeok() {
   // (1) 달력 초기화 구성 함수
   const initDallyeok = () => {
     // 1) 변수 초기화
+    // -> 날짜배열 초기화: splice(시작순번,개수)
+    // -> 배열변수.splice(0) 첫배열부터 모두 지움
+    dateSet.splice(0);
+    // html 코드 초기화
+    hcode = "";
 
     // 2) 변수 할당
     // 현재년
@@ -115,19 +120,81 @@ function makeDallyeok() {
     }
 
     // 날짜 배열갓 확인
-    myFn.cs("dateSet" + dateSet);
+    // myFn.cs("dateSet" + dateSet);
 
     // 6) 날짜배열로 날짜태그 구성하기
     // 구성요건 : 7일 + 6주 = 42
     for(let i = 0; i < 42; i ++){
-      hcode+= `<div class="date">${dateSet[i]}</div>`;
+      if (
+        // 년, 월, 일 모두 일치하는 오늘만 표시
+        // (1) 오늘날짜 === 배열값 날짜 AND
+        today.getDate() === dateSet[i] &&
+        // (2) 현재달 === 선택달 AND
+        today.getMonth() === currDate.getMonth() &&
+        // (3) 현재년도 === 선택년도
+        today.getFullYear() === currDate.getFullYear()
+      )
+      {
+        hcode+= `<div class="date today">${dateSet[i]}</div>`;
+      }
+      else {
+        hcode+= `<div class="date">${dateSet[i]}</div>`;
+      }
+      
     } // for //
 
     // 날짜태그 출력
     dates.innerHTML = hcode;
 
-  }; // initDallyeok //
+    //⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️//
+    // 7) 날짜정보를 사용하도록 세팅하기
+    // (1) 대상선정 : .date -> 위에서 새로 담겼으므로 새로 읽음
+    let newDate = myFn.qsa('.date');
+    // console.log(newDate);
 
-  // 초기 세팅 함수 호출
-  initDallyeok();
+    // (2) 각 날짜 .date요소에 링크 설정하기
+    newDate.forEach(el => {
+      // 각 .date요소에 클릭 이벤트 설정하기
+      myFn.addEvt(el, 'click', () => {
+        // 1. 년도읽기
+        let nowY = yearTit.innerText;
+        // 2. 월읽기
+        let nowM = monthTit.innerText;
+        // 3. 날짜읽기
+        let nowD = el.innerText;
+        // 4. 이전달/다음달 구분하기
+        let isSpan = myFn.qsEl(el, 'span');
+        console.log('span??', isSpan);
+
+      }); // addEvt //
+    }); // forEach 메서드 //
+
+  }; // initDallyeok //
+  
+  // (2) 함수만들기 //
+  // 이전 / 다음 달력 출력하기 함수
+
+  const chgCalendar = (num) => {
+    // num (1이면 다음, -1이면 이전)
+
+    console.log("달력변수", chgCalendar);
+    // 이전 / 다음달로 변경하여 initDallueok() 함수 호출
+    currDate.setMonth(currDate.getMonth()+num);
+    // getMonth() 월 가져오기 / setMonth() 월 세팅하기
+
+    // 달력초기화 함수 다시 호출하기
+    initDallyeok();
+    
+  }; // chgCalendar //
+
+    // 3. 이벤트 설정하기
+    // (1) 이전버튼에 함수 연결하기 : 달을 빼기 위해 -1을 전달한다.
+    myFn.addEvt(myFn.qs('.btnL'),'click',() => chgCalendar(-1));
+    // (2) 다음버튼에 함수 연결하기 : 달을 더하기 위해 +1을 전달한다.
+    myFn.addEvt(myFn.qs('.btnR'),'click',() => chgCalendar(+1));
+
+    // 4. 초기 세팅 함수 호출
+    initDallyeok();
+
 } /////////////// makeDallyeok함수 ////////////
+
